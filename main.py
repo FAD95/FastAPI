e5dab4d69ad7415287ca053234f2b930c080dbe4
FAD95 <feescorciam@protonmail.com>
2022-01-16 17:30:51 -0500

    Post and model validation

--- main.py
+++ main.py
@@ -1,7 +1,27 @@
-from fastapi import FastAPI
+# Python 
+from typing import Optional
+
+# Pydantic
+from pydantic import BaseModel
+
+# FastAPI
+from fastapi import FastAPI, Body
 
 app = FastAPI()
+
+class Person(BaseModel):
+    first_name: str
+    last_name: str
+    age: int
+    address: Optional[str] = None
+    phone: Optional[str] = None
+    hair_color: Optional[str] = None
+    is_married: Optional[bool] = None
 
 @app.get("/")
 def home():
     return {"message": "Hello World"}
+
+@app.post("/person/new")
+def create_person(person: Person = Body(...)):
+    return person.dict()

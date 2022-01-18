40a923d52c49a953380e25f7cf8178a1e9dd5b39
FAD95 <feescorciam@protonmail.com>
2022-01-18 18:56:03 -0500

    validation_path

--- main.py
+++ main.py
@@ -5,10 +5,10 @@ from typing import Optional
 from pydantic import BaseModel
 
 # FastAPI
-from fastapi import FastAPI, Body, Query
+from fastapi import FastAPI
+from fastapi import Body, Query, Path
 
 app = FastAPI()
-
 class Person(BaseModel):
     first_name: str
     last_name: str
@@ -17,6 +17,7 @@ class Person(BaseModel):
     phone: Optional[str] = None
     hair_color: Optional[str] = None
     is_married: Optional[bool] = None
+
 
 @app.get("/")
 def home():
@@ -33,7 +34,33 @@ def create_person(person: Person = Body(...)):
 
 @app.get("/person/detail")
 def show_person(
-    name: Optional[str] = Query(None, min_length=1, max_length=50, regex="^[A-Z]+[a-zA-Z]+$"), 
-    age: int = Query(..., ge=15, le=150) #Also can use gt (>) and lt (<)
+    name: Optional[str] = Query(
+        None, 
+        min_length=1, 
+        max_length=50, 
+        regex="^([A-Z])\w",
+        tile="Person Name",
+        description="This is the person name. It must be capitalised and between 1 and 50 characters."
+        ), 
+    age: int = Query(
+        ...,
+        title="Person Age", 
+        description="This is the person age. It must be an integer and is required.",
+        ge=15, 
+        le=150
+        ) #Also can use gt (>) and lt (<)
     ):
     return {name: age}
+
+# Validations: Path Parameters
+
+@app.get("/person/detail/{person_id}")
+def show_person(
+    person_id: int = Path(
+        ..., 
+        title="Person ID",
+        description="This is the person ID. It must be an integer greater than 0 and is required.",
+        gt=0
+        )
+    ):
+    return {person_id: "It exists!"}
